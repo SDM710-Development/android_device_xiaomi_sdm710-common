0a7c25436e5bc2b2af180e42680a502092cc88a5
LuK1337 <priv.luk@gmail.com>
2019-10-06 21:31:41 +0200

    sdm710-common: Fix qcrilmsgtunnel dependencies
    * Back in Android P, android.hidl.manager-V1.0-java was
      always loaded thanks to being in BOOTCLASSPATH.
      However on Q it's not there anymore and qcrilmsgtunnel
      was kinda prepared for that b-but they used wrong
      library name 'android.hidl.manager@1.0-java' vs
      'android.hidl.manager-V1.0-java' so it didn't really
      work...
    
    Change-Id: I0f8db83b955c133b0089d89e8fb5f006d6873a96

--- extract-files.sh
+++ extract-files.sh
@@ -35,9 +35,6 @@ function blob_fixup() {
         system_ext/etc/permissions/telephonyservice.xml)
             sed -i "s/\/product\/framework\//\/system_ext\/framework\//g" "${2}"
             ;;
-        system_ext/etc/permissions/qti_libpermissions.xml)
-            sed -i "s/name=\"android.hidl.manager-V1.0-java/name=\"android.hidl.manager@1.0-java/g" "${2}"
-            ;;
         system_ext/lib64/libdpmframework.so)
             sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
             ;;
