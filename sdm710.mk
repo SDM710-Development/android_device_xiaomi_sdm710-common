d57df8886c5ce6869a005c8a73bbf024098f9928
YumeMichi <do4suki@gmail.com>
2020-02-19 04:24:29 +0800

    sdm710-common: Switch to OSS audio HAL

--- sdm710.mk
+++ sdm710.mk
@@ -78,11 +78,20 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
+    audio.primary.sdm710 \
     audio.r_submix.default \
     audio.usb.default \
+    liba2dpoffload \
+    libbatterylistener \
+    libcomprcapture \
+    libexthwplugin \
+    libhdmiedid \
+    libhfp \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
+    libsndmonitor \
+    libspkrprot \
     libtinycompress \
     libvolumelistener \
     tinymix
