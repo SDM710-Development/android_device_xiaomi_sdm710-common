74f2f8e64f6d18eddde1087c161d5f617015e761
Bruno Martins <bgcngm@gmail.com>
2020-10-09 12:19:37 +0100

    sdm710-common: power: Adapt extension to new aidl power HAL
    Change-Id: I58ac49957d8d28ad904912986d89ac8db4820ff9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,7 +122,7 @@ TARGET_USES_MKE2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Power
-TARGET_POWER_SET_FEATURE_LIB := libpower_feature.xiaomi_sdm710
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
