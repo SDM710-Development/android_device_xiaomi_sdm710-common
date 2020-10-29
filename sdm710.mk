b6657711ae22537277e47e4d358afde1abd72b65
Alex Damaratski <alexeydomoratsky1@gmail.com>
2020-10-29 11:07:34 +0300

    sdm710-common: Compile thermal and VR HALs from source

--- sdm710.mk
+++ sdm710.mk
@@ -281,7 +281,8 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.sdm710
 
 # Trust HAL
 PRODUCT_PACKAGES += \
@@ -303,7 +304,8 @@ PRODUCT_PACKAGES += \
 # VR
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
-    android.hardware.vr@1.0-service
+    android.hardware.vr@1.0-service \
+    vr.sdm710
 
 # WiFi
 PRODUCT_PACKAGES += \
