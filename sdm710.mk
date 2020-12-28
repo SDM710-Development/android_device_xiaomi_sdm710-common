d1ebba1d421db294a49c72895ca64e0161e9ed3f
uixdess <alexeydomoratsky1@gmail.com>
2020-12-28 11:44:42 +0300

    sdm710-common: configs: add irqbalance

--- sdm710.mk
+++ sdm710.mk
@@ -211,6 +211,10 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# IRQ
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sdm710
