SDM710-Development/android_device_xiaomi_sdm710-common

commit 47e2898e79022fa5337e9c36649ac97f1d057a29

sdm710-common: move partition sizes to device specific tree
Grus, pyxis and sirius have different partitions.

grus						pyxis						sirius
/dev/block/by-name/boot:134217728		/dev/block/by-name/boot:134217728		/dev/block/by-name/boot:67108864
/dev/block/by-name/dtbo:25165824		/dev/block/by-name/dtbo:25165824		/dev/block/by-name/dtbo:8388608
/dev/block/by-name/system:3758096384		/dev/block/by-name/system:3758096384		/dev/block/by-name/system:3221225472
/dev/block/by-name/vendor:1610612736		/dev/block/by-name/vendor:1610612736		/dev/block/by-name/vendor:3221225472
/dev/block/by-name/recovery:67108864		/dev/block/by-name/recovery:67108864		/dev/block/by-name/recovery:67108864

Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
Change-Id: Ibe7fe0d7ae569d2648e5db197a704a69739e376d

BoardConfigCommon.mk
@@ -109,13 +109,10 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 TARGET_LMKD_STATS_LOG := true
 
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 57453555712
-BOARD_VENDORIMAGE_PARTITION_SIZE := 1073741824
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 

commit c3a689b33741067214875c222761e40fdd859584

sdm710-common: add back vendor fs type param
Recent commit removed parameters for sizes of uncommon partitions.
Unfortunately it also removed vendor filesystem type parameters
that resulted in weird error messages like:

device/qcom/sepolicy_vndr/legacy/vendor/common/hal_display_color.te:30:ERROR 'Duplicate declaration of type' at token ';' on line 72732:
type hal_display_color_default, domain;
# Define domain
checkpolicy:  error(s) encountered while parsing configuration

Fixes: 47e2898 ("sdm710-common: move partition sizes to device specific tree")
Change-Id: Ib4d3a19a5a05fc67d302c9d19a78750945c5bfc7
Signed-off-by: Ivan Vecera <ivan@cera.cz>

BoardConfigCommon.mk
@@ -113,6 +113,7 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 57453555712
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 