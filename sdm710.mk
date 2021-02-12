62d72958d75556bfef3bd0562a73057a83eca8e2
Ivan Vecera <ivan@cera.cz>
2021-02-12 11:26:54 +0100

    sdm710-common: re-add TZ based assertions
    This commit just revert commit b8d3ef2eb028389169dc0b1b68cfbeb42fa080e9
    that removed this functionality completely. The follow-up commits
    fixes the functionality.
    
    Based on https://github.com/LineageOS/android_device_xiaomi_sdm845-common/tree/lineage-17.1/recovery
    
    Change-Id: Ib3280e3ccdc7891186f15915197df5fa59ac8a81
    Signed-off-by: Ivan Vecera <ivan@cera.cz>

--- sdm710.mk
+++ sdm710.mk
@@ -291,6 +291,10 @@ PRODUCT_PACKAGES += \
     libjson \
     librmnetctl
 
+# Recovery
+PRODUCT_PACKAGES += \
+    librecovery_updater_xiaomi
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
