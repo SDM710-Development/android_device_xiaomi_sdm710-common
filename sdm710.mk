7d698c7ff5785ff484f861146e7fc7dcba366474
Albert I <kras@raphielgang.org>
2021-04-01 05:49:47 +0800

    sdm710-common: Ship proper Google Codec2 support
    * Update media_codecs_performance from LA.UM.9.8.r1-02700-SDM710.0
    
    * Drop audio OMX prioritization to match non-generic ODM variant
    
    * Under this configuration, Codec2 will only be used with audio decoding as
      well as video decoding in SW mode while HW mode will continue to use OMX
    
    * com.android.media.swcodec and libsfcodec_plugin are built by default
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -119,6 +119,12 @@ PRODUCT_PACKAGES += \
     Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
+# Codec2
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
