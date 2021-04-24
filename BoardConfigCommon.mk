b7843f28bbb26ad4a0d11aec52e6daaa76769250
Bruno Martins <bgcngm@gmail.com>
2021-04-24 15:48:15 +0100

    sdm710-common: Build disabled VBMeta image for all targets
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: I73ec392fd55fdaff5c5c7d3e3e19ef03dbf49dce

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -154,6 +154,15 @@ BOARD_ROOT_EXTRA_SYMLINKS += \
     /data/tombstones:/tombstones \
     /mnt/vendor/persist:/persist
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
 # WiFi
 BOARD_WLAN_DEVICE := qcwcn
 BOARD_HOSTAPD_DRIVER := NL80211
