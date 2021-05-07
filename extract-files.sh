787d5149d03966b4862c38465be107b094660c07
Ivan Vecera <ivan@cera.cz>
2021-05-07 18:51:22 +0200

    sdm710-common: Remove dependency on vendor.qti.hardware.display.mapper@2/3
    The sensors.ssc.so blob from MIUI 12.5 references
    vendor.qti.hardware.display.mapper@{2,3}.0.so that is not necessary.
    Remove these references by patchelf.
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: Ia68402c45ce482b4208c78c897dba111d4bdd39c

--- extract-files.sh
+++ extract-files.sh
@@ -27,6 +27,10 @@ function blob_fixup() {
             ${PATCHELF} --remove-needed vendor.qti.hardware.display.mapper@2.0.so "${2}"
             ${PATCHELF} --remove-needed vendor.qti.hardware.display.mapper@3.0.so "${2}"
             ;;
+        vendor/lib*/sensors.ssc.so)
+            ${PATCHELF} --remove-needed vendor.qti.hardware.display.mapper@2.0.so "${2}"
+            ${PATCHELF} --remove-needed vendor.qti.hardware.display.mapper@3.0.so "${2}"
+            ;;
     esac
 }
 
