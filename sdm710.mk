a7c858f7ac8aac85d1a86fa0dd64da71bf3adb61
Lucchetto <lucchetto.tie@live.com>
2021-11-03 19:11:57 +0100

    sdm710-common: Build libavservices_minijail.vendor
    * Required by new IMS stack
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: Ia8fbef359c0d9e47371d62adc56da06c41c526bf

--- sdm710.mk
+++ sdm710.mk
@@ -216,6 +216,7 @@ PRODUCT_PACKAGES += \
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
+    libavservices_minijail.vendor \
     libc2dcolorconvert \
     libOmxAacEnc \
     libOmxAmrEnc \
