297d38f769cec5ed7d57776d32edad00c459dc8a
Ivan Vecera <ivan@cera.cz>
2021-11-03 19:04:27 +0100

    sdm710-common: Take WFD and telephony permissions from stock
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I93651848c430dd58b4dc1389ea1f1d8b47f5fa33

--- sdm710.mk
+++ sdm710.mk
@@ -270,8 +270,7 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
-    $(LOCAL_PATH)/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
 # Radio
 PRODUCT_PACKAGES += \
