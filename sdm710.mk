5d849c634ad8405a4a84d9e82acbe097589d94aa
Ivan Vecera <ivan@cera.cz>
2021-11-07 19:58:37 +0100

    sdm710-common: fod: Commonize enablement conditionally
    Pyxis and grus supports Fod so commonize its support conditionally.
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I5dd9b373f255daa6d029cc353ca2a3471c118603

--- sdm710.mk
+++ sdm710.mk
@@ -188,6 +188,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm710
 
+ifeq ($(TARGET_HAS_FOD),true)
+PRODUCT_PACKAGES += \
+    libkeymaster_messages.vendor \
+    libkeymaster_portable.vendor \
+    vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_sdm710
+
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+endif
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
