7f3bf0823a25946ef1508ebb98a12f977c072821
Ivan Vecera <ivan@cera.cz>
2021-11-29 11:38:28 +0100

    sdm710-common: releasetools: Ship and flash firmware blobs
    Ship firmware blobs provided by device tree in proprietary-firmware.txt
    file and flash them according provided filesmap.
    
    The filesmap file should be stored in radio/ directory in device
    tree with the following format:
    ```
    <filename> <partition>
    ```
    
    Example:
    ```
    keymaster64.mbn /dev/block/bootdevice/by-name/keymaster
    ```
    
    Signed-off-by: Ivan Vecera <ivan@cera.cz>
    Change-Id: I8b3476f6ba046acedd8e262efbec14f9182e2816

--- releasetools.py
+++ releasetools.py
@@ -17,6 +17,71 @@
 import common
 import re
 
+fw_files = {}
+
+# Parse filesmap file containing firmware residing places
+def LoadFilesMap(zip, name="RADIO/filesmap"):
+  try:
+    data = zip.read(name)
+  except KeyError:
+    print "Warning: could not find %s in %s." % (name, zip)
+    data = ""
+  d = {}
+  for line in data.split("\n"):
+    line = line.strip()
+    if not line or line.startswith("#"):
+      continue
+    pieces = line.split()
+    if not (len(pieces) == 2):
+      raise ValueError("malformed filesmap line: \"%s\"" % (line,))
+    d[pieces[0]] = pieces[1]
+  return d
+
+# Read firmware images from target files zip
+def GetFwFiles(z):
+  out = {}
+  for info in z.infolist():
+    f = info.filename
+    if f.startswith("RADIO/") and (f.__len__() > len("RADIO/")):
+      fn = f[6:]
+      if fn.startswith("filesmap"):
+        continue
+      data = z.read(f)
+      out[fn] = common.File(f, data)
+  return out
+
+# Prepare firmware files and verify them
+def PrepareFwFiles(info, zip_file):
+  print "Loading firmware files map..."
+  filesmap = LoadFilesMap(zip_file)
+  if filesmap == {}:
+    print "warning radio-update: no or invalid filesmap file found"
+    return False
+
+  print "Loading firmware files..."
+  tgt_files = GetFwFiles(zip_file)
+  if tgt_files == {}:
+    print "warning radio-update: no firmware images in input target_files"
+    return False
+
+  global fw_files
+
+  print "Preparing firmware files..."
+  for fn in tgt_files:
+    dest = filesmap[fn]
+    if dest is None:
+      continue
+
+    tf = tgt_files[fn]
+    f = "firmware-update/" + fn
+    common.ZipWriteStr(info.output_zip, f, tf.data)
+    fw_files[f] = dest
+
+  return True
+
+def IncrementalOTA_VerifyEnd(info):
+  return PrepareFwFiles(info, info.target_zip)
+
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
   return
@@ -43,9 +108,18 @@ def AddImage(info, basename, dest):
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
+def InstallFwImages(script, files):
+  for f in files:
+    dest = files[f]
+    script.Print("Patching {} firmware unconditionally...".format(dest.split('/')[-1]))
+    script.AppendExtra('package_extract_file("%s", "%s");' % (f, dest))
+  return
+
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  if PrepareFwFiles(info, info.input_zip):
+    InstallFwImages(info.script, fw_files)
   return
 
 def AddTrustZoneAssertion(info, input_zip):
