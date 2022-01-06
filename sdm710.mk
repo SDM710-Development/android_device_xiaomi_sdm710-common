dotOS-Devices/device_lenovo_jd2019-common

sdm710-common: livedisplay: Uprev to 2.1
Change-Id: I98b2ea16ed342339ee652d5cb79dcfd22f8c0a0d

sdm710.mk
@@ -231,7 +231,7 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.lenovo_sdm710
+    vendor.lineage.livedisplay@2.1-service.lenovo_sdm710
 
 # Media
 PRODUCT_PACKAGES += \

SDM710-Development/android_device_xiaomi_sdm710-common

sdm710.mk
@@ -260,7 +260,7 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.xiaomi_sdm710
+    vendor.lineage.livedisplay@2.1-service.xiaomi_sdm710
 
 # Media
 PRODUCT_PACKAGES += \